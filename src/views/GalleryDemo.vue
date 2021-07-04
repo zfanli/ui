d673cdd817e3e8a34557320aa23e147b02f7d431
Richard <zfanli@outlook.com>
2021-07-04 22:38:15 +0800

    Add demo of the gallery component

---
+++ src/views/GalleryDemo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <gallery :images="images"></gallery>
+    <div class="scene"></div>
+  </div>
+</template>
+
+<script>
+import Gallery from "../components/Gallery.vue";
+
+export default {
+  name: "GalleryDemo",
+  components: { Gallery },
+  data: () => {
+    const images = [];
+    for (let i = 1; i <= 36; i++) {
+      images.push({
+        src: require(`../assets/images/parallax/p${(i % 4) + 1}.jpg`),
+        span: (i % 4) + 1,
+      });
+    }
+    return { images };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.scene {
+  height: 100vh;
+}
+</style>
