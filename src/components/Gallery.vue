2f0e99ff7d43ade33099d0a9db2c01942f0d7c9f
Richard <zfanli@outlook.com>
2021-07-08 00:27:55 +0800

    Update image viewer

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -18,9 +18,16 @@
       <div
         v-for="(img, i) in images"
         :key="i"
-        :class="['item', 'span' + img.span, itemClass]"
-        :style="{ 'background-image': `url(${img.src})` }"
-      ></div>
+        :class="[
+          'item',
+          'span' + img.span,
+          itemClass,
+          curr === i ? 'viewer' : '',
+        ]"
+        @click="viewer(i)"
+      >
+        <img :src="img.src" alt="image" />
+      </div>
     </div>
 
     <v-progress-linear
@@ -34,6 +41,18 @@
       bottom
       absolute
     ></v-progress-linear>
+
+    <div class="viewer-panel" v-show="curr !== -1">
+      <v-icon class="action-icon closable" @click="closeViewer">
+        mdi-close
+      </v-icon>
+      <v-icon class="action-icon prev" @click="showImage(-1)">
+        mdi-chevron-double-left
+      </v-icon>
+      <v-icon class="action-icon next" @click="showImage(1)">
+        mdi-chevron-double-right
+      </v-icon>
+    </div>
   </div>
 </template>
 
@@ -49,6 +68,7 @@ ScrollTrigger.defaults({
 
 export default {
   name: "Gallery",
+
   props: {
     images: {
       type: Array,
@@ -63,12 +83,17 @@ export default {
       default: "red",
     },
   },
+
   data: () => ({
     cellHeight: 0,
     cellWidth: 0,
     timeline: null,
     progress: 0,
+    curr: -1,
+    viewerTriggerImageBackup: null,
+    viewerTriggerImage: null,
   }),
+
   mounted() {
     this.onResize();
     window.addEventListener("resize", this.onResize);
@@ -98,14 +123,106 @@ export default {
   },
   destroyed() {
     window.removeEventListener("resize", this.onResize);
+    window.removeEventListener("scroll", this.disableScrolling);
   },
   methods: {
     onResize() {
       const { clientHeight, clientWidth } = this.$refs.wrapper;
       this.cellHeight = this.$vuetify.breakpoint.mobile
-        ? Math.ceil((clientWidth / 2) * 0.5625)
+        ? Math.ceil((clientWidth / 2) * 0.4)
         : Math.ceil(clientHeight / 4);
       this.cellWidth = Math.ceil(this.cellHeight * 0.45);
+    },
+
+    viewer(i) {
+      // ignore if image viewer is already displayed
+      if (this.curr !== -1) return;
+
+      // generate a scroll handler for prevent scrolling while the viewer is showing
+      this.disableScrolling = (() => {
+        const offset = window.pageYOffset;
+        return () => window.scrollTo(0, offset);
+      })();
+      window.addEventListener("scroll", this.disableScrolling);
+
+      // keep current viewed index of image
+      this.curr = i;
+
+      // prepare animation targets
+      const target = this.$refs.body.children[i];
+      const image = target.children[0];
+      const { imageWidth, imageHeight } = this.calculate(image);
+
+      this.viewerAnime = [
+        gsap.to(target, {
+          width: window.innerWidth,
+          height: window.innerHeight,
+          position: "relative",
+          top: -target.offsetTop,
+          left: -target.offsetLeft,
+          zIndex: 9999,
+          display: "flex",
+          duration: 0.25,
+          ease: "none",
+        }),
+        gsap.to(
+          image,
+          {
+            width: imageWidth,
+            height: imageHeight,
+            minWidth: "unset",
+            duration: 0.5,
+            // ease: "none",
+          },
+          "-=0.25"
+        ),
+      ];
+
+      this.viewerTriggerImageBackup = image.src;
+      this.viewerTriggerImage = image;
+    },
+
+    calculate(image) {
+      const imageRatio = image.width / image.height;
+      let imageHeight = window.innerHeight - 50,
+        imageWidth = imageHeight * imageRatio;
+
+      if (imageWidth > window.innerWidth - 50) {
+        imageWidth = window.innerWidth - 50;
+        imageHeight = imageWidth / imageRatio;
+      }
+
+      return { imageWidth, imageHeight };
+    },
+
+    showImage(i) {
+      const next = this.curr + i;
+      if (next < 0 || next >= this.images.length) return;
+      const image = this.$refs.body.children[next].children[0];
+      const { imageWidth, imageHeight } = this.calculate(image);
+
+      gsap.to(this.viewerTriggerImage, {
+        width: imageWidth,
+        height: imageHeight,
+      });
+      this.viewerTriggerImage.src = image.src;
+      this.curr = next;
+    },
+
+    closeViewer() {
+      this.curr = -1;
+      window.removeEventListener("scroll", this.disableScrolling);
+      for (let an of this.viewerAnime) an.reverse();
+      if (this.viewerTriggerImage.src !== this.viewerTriggerImageBackup) {
+        gsap
+          .timeline()
+          .to(this.viewerTriggerImage, {
+            opacity: 0,
+            onComplete: () =>
+              (this.viewerTriggerImage.src = this.viewerTriggerImageBackup),
+          })
+          .to(this.viewerTriggerImage, { opacity: 1 });
+      }
     },
   },
 };
@@ -120,6 +237,7 @@ export default {
   overflow: hidden;
   background-size: cover;
   background-position: center;
+  position: relative;
 
   .gallery-body.grid.gallery-row {
     height: 100vh;
@@ -166,11 +284,21 @@ export default {
   }
 
   .item {
-    background-color: lightpink;
+    background-color: white;
     cursor: pointer;
     transition: transform 0.25s ease, box-shadow 0.25s ease;
-    background-size: cover;
-    background-position: center;
+    position: relative;
+    overflow: hidden;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      min-width: 100%;
+      height: 100%;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+    }
 
     &:hover {
       transform: translateY(-5px);
@@ -178,11 +306,58 @@ export default {
         0px 8px 10px 1px rgb(0 0 0 / 14%), 0px 3px 14px 2px rgb(0 0 0 / 12%);
     }
 
+    &.viewer {
+      cursor: unset;
+
+      &:hover {
+        transform: none;
+        box-shadow: none;
+      }
+
+      img {
+        height: 100%;
+        min-width: unset;
+        width: auto;
+      }
+    }
+
     // disable hover styles on mobile devices due to the inconsistency of user experiences
     @media #{map-get($display-breakpoints, 'sm-and-down')} {
       &:hover {
         transform: none;
         box-shadow: none;
+      }
+    }
+  }
+
+  .viewer-panel {
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+
+    .action-icon {
+      position: absolute;
+      font-size: 2rem;
+      cursor: pointer;
+      color: white;
+      text-shadow: 0 0 10px black;
+
+      &.closable {
+        right: 1.5rem;
+        top: 1rem;
+      }
+
+      &.prev {
+        top: 50%;
+        left: 1.5rem;
+      }
+
+      &.next {
+        top: 50%;
+        right: 1.5rem;
       }
     }
   }
