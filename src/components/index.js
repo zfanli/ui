d727f1494f30f489238dac624aab1837ab15905b
Richard <zfanli@outlook.com>
2021-07-31 22:14:23 +0800

    Register components globally

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Album from "./Album";
+import CustomNav from "./CustomNav";
+import Gallery from "./Gallery";
+import Loading from "./Loading";
+import Navigation from "./Navigation";
+import parallax from "./Parallax";
+import ProgressLinear from "./ProgressLinear";
+import Timeline from "./Timeline";
+
+export default {
+  Album,
+  CustomNav,
+  Gallery,
+  Loading,
+  Navigation,
+  parallax,
+  ProgressLinear,
+  Timeline,
+};
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
+import components from "./components";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import toolbox from "./plugins/toolbox";
 
 Vue.config.productionTip = false;
 Vue.use(toolbox);
+
+// the v2 way to register components globally
+Object.entries(components).forEach(([key, value]) => {
+  Vue.component(key, value);
+});
 
 new Vue({
   router,
